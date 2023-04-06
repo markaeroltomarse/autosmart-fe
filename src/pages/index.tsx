2b2df4f8a69225cf9f34ff8358b225eb5e1af8a4
markaeroltomarse <marktomarse@gmail.com>
2023-04-06 21:13:00 +0800

    home page product list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,11 +34,30 @@ export default function Home() {
         <h1 className="text-2xl">
           Products {productsState.isSuccess && productsState?.data.data.length}
         </h1>
-        <div className="flex">
+        <div className="flex gap-2 flex-row flex-wrap border">
           {productsState.isSuccess &&
             productsState?.data.data.map((product: any) => (
-              <div className="border p-5 rounde" key={product.id}>
-                {product.name}
+              <div
+                className="border p-5 rounded bg-white flex-wrap w-[24.2%]"
+                key={product.id}
+              >
+                <Image
+                  src={
+                    'https://cdn.shopify.com/s/files/1/0580/3245/5858/products/10-pc-chickenjoy-bucket.jpg?v=1635459211&width=1080'
+                  }
+                  alt="product"
+                  width={150}
+                  height={150}
+                />
+                <div className="flex justify-between">
+                  <div>{product.name}</div>
+                  <div>
+                    <span className="text-slate-500 line-through">
+                      ${product.price}
+                    </span>{' '}
+                    ${product.price - product.discount}
+                  </div>
+                </div>
               </div>
             ))}
         </div>
